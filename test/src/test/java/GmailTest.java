1327e8778b7190b73187e3b31b999dd53152db10
VavrukIryna <irynavavruk@gmail.com>
2019-08-06 16:53:57 +0300

    add task 4. add property file and selenide

--- test/src/test/java/GmailTest.java
+++ test/src/test/java/GmailTest.java
@@ -1,23 +1,40 @@
+import com.codeborne.selenide.Browsers;
+import com.codeborne.selenide.WebDriverRunner;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import static com.codeborne.selenide.Browsers.FIREFOX;
+import static com.codeborne.selenide.Selenide.*;
 
 public class GmailTest {
-    private static final Logger LOGGER = Logger.getLogger("GmailTest.class");
-    @Test
-    public void writtingTestInGmail(){
-        LOGGER.info("GMAIL TEST LOGGER");
-        open("http://gmail.com");
-        $(By.xpath("//*[@type ='email']]")).setValue("test.iryna.mail");
-        $(By.xpath("//*[@type =\"password\"]]")).setValue("testmail");
-        $(By.xpath("//*[@class = 'T-I J-J5-Ji T-I-KE L3']")).click();
-        $(By.xpath("//*[@name = 'to']")).setValue("test.iryna.mail@gmail.com");
-        $(By.xpath("//*[@name = 'subjectbox']")).setValue("mail subject");
-        $(By.xpath("//*[@id = ':qb']")).setValue("mail text");
-        $(By.xpath("//*[text() = 'Send']")).click();
 
+    private static final Logger LOGGER = Logger.getLogger("GmailTest.class");
+
+    @Test
+    public void writtingTestInGmail() {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            System.setProperty("selenide.browser", prop.getProperty("chromeDriver"));
+
+            LOGGER.info("GMAIL TEST LOGGER");
+            open("http://gmail.com");
+            sleep(1000);
+            $(By.xpath("//*[@type ='email']]")).setValue("test.iryna.mail");
+            $(By.xpath("//*[@type =\"password\"]]")).setValue("testmail");
+            $(By.xpath("//*[@class = 'T-I J-J5-Ji T-I-KE L3']")).click();
+            $(By.xpath("//*[@name = 'to']")).setValue("test.iryna.mail@gmail.com");
+            $(By.xpath("//*[@name = 'subjectbox']")).setValue("mail subject");
+            $(By.xpath("//*[@id = ':qb']")).setValue("mail text");
+            $(By.xpath("//*[text() = 'Send']")).click();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- test/src/test/java/GoogleTest.java
+++ test/src/test/java/GoogleTest.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -14,14 +18,24 @@ public class GoogleTest {
 
     private static final Logger LOGGER = Logger.getLogger("GoogleTest.class");
     @Test
-    public void checkSearchingWordAtGoogle(){
-        LOGGER.info("NEW TEST LOGGER");
-        open("http://google.com");
-        $(By.name("q")).setValue("selenide").pressEnter();
-      //  $$("#ires  .g" ).shouldHave(size(10));
-      //  $("#ires .g").shouldHave(text("selenide.org"));
-        Assert.assertEquals(1,1);
+    public void checkSearchingWordAtGoogle() throws InterruptedException {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            System.setProperty("selenide.browser", prop.getProperty("firefoxDriver"));
+
+
+            LOGGER.info("NEW TEST LOGGER");
+            open("http://google.com");
+            sleep(1000);
+            $(By.name("q")).setValue("selenide").pressEnter();
+            sleep(1000);
+            //  $$("#ires  .g" ).shouldHave(size(10));
+            //  $("#ires .g").shouldHave(text("selenide.org"));
+            Assert.assertEquals(1, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
-
-
-}
+    }

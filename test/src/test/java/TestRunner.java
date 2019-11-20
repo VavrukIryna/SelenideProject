17b7a7cab2f1c02fb3edb27ba233ef28149ecc3d
Iryna_vavruk@epam.com <guard4388**>
2019-11-21 00:21:52 +0200

    fix allure

--- test/src/test/java/TestRunner.java
+++ test/src/test/java/TestRunner.java
@@ -2,7 +2,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources",
+        features = "src/test/java/feature",
         glue = {"steps"},
         plugin = {"pretty", "html:target/cucumber"}
 )
